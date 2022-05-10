3424e6f4f67ef0e7117e70da4b879cd2d3a24cb7
Minhhoang112331 <minhnhpd02469@gmail.com>
2022-05-10 20:11:10 +0700

    Chapter 5: Filling the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,12 @@ module ApplicationHelper
     base_title = "Ruby on Rails Tutorial Sample App"
     
     page_title.blank? ? base_title : page_title + " | " + base_title 
+	def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
   end
 end
