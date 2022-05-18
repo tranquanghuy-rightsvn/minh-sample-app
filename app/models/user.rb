ed8d0f8b70f32e6749e965996e468e532c9abecd
Minhhoang112331 <minhnhpd02469@gmail.com>
2022-05-18 19:41:27 +0700

    Chapter7: Sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
