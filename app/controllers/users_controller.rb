b189a31f4e585cf45a3973676029008c511f0a53
Minhhoang112331 <minhnhpd02469@gmail.com>
2022-05-20 19:38:43 +0700

    Chapter 9:URD users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
   def index
-    @users = User.all
+    @users = User.page(params[:page]).per 10
   end
 
   def show
@@ -21,9 +25,43 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = I18n.t ".messages.user.profile"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = I18n.t ".messages.user.deleted"
+    redirect_to users_url
+  end
+
   private
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = I18n.t ".messages.user.login"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user? @user
+  end
 
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_20_103228) do
+ActiveRecord::Schema.define(version: 2022_05_22_145833) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_05_20_103228) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
